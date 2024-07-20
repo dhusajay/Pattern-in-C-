cd8c190f089ab76e5ecb36158282de7725b17257
dhusajay <139203497+dhusajay@users.noreply.github.com>
2024-07-20 12:40:11 +0530

    1_Right_Triangle_Star_Pattern.c

--- 1_Right_Triangle_Star_Pattern.c
+++ 1_Right_Triangle_Star_Pattern.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int rows, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 1; i <= rows; i++) {
+        // Print stars in increasing order
+        for (j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
